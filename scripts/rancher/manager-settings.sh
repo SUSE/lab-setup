12a371f21e1a165987079ebb84ab7d5c6bcc5b42
olegvorobiov <olegvorobyov90@gmail.com>
2024-09-04 18:08:58 -0400

    reverted some changes, modified upadate serverurl function

--- scripts/rancher/manager-settings.sh
+++ scripts/rancher/manager-settings.sh
@@ -5,20 +5,14 @@
 # Set Rancher Server URL setting
 # Arguments:
 #   Rancher URL
-#   Token
 # Examples:
-#   rancher_update_serverurl https://rancher.random_string.geek xxxxx
+#   rancher_update_serverurl https://rancher.random_string.geek
 #######################################
 rancher_update_serverurl() {
   local rancherUrl=$1
-  local token=$2
 
   echo "Sets Rancher URL in settings..."
-  curl -s -k -H "Authorization: Bearer $token" \
-    -H 'Content-Type: application/json' \
-    -X PUT \
-    -d '{
-          "value": "'"$rancherUrl"'"
-        }' \
-    "$rancherUrl/v3/settings/server-url"
+  kubectl patch settings.management.cattle.io server-url --type='merge' --patch '{                                                                                                                                                ─╯
+"value": "'$rancherUrl'"
+}'
 }
