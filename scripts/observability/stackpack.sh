6c62a371643876495fcb803bec534a5b6544e329
Jeroen van Erp <jeroen.vanerp@suse.com>
2024-09-17 11:25:54 +0200

    Return instead of global vars
    Signed-off-by: Jeroen van Erp <jeroen.vanerp@suse.com>

--- scripts/observability/stackpack.sh
+++ scripts/observability/stackpack.sh
@@ -43,9 +43,6 @@ observability_check_stackpack() {
 
   local stackpacks=$(/usr/local/bin/sts stackpack list-instances --name kubernetes-v2 -o json --url $url --service-token $service_token)
   local stackpack_id=$(echo $stackpacks | jq -r '.instances[] | select(.config.kubernetes_cluster_name == "'$cluster_name'") | .id')
-  if [ ! -z "$stackpack_id" ];  then
-    return 0
-  else
-    return 1
-  fi
+  [[ ! -z "$stackpack_id" ]]
+  return
 }
