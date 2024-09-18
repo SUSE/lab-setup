923918cd7276b5071b41604b2f949d4b829dc949
Jeroen van Erp <jeroen.vanerp@suse.com>
2024-09-18 10:54:11 +0200

    Fix negation of cli check
    Signed-off-by: Jeroen van Erp <jeroen.vanerp@suse.com>

--- scripts/observability/cli.sh
+++ scripts/observability/cli.sh
@@ -4,7 +4,7 @@
 # Install the SUSE Observability CLI
 #######################################
 observability_install_cli() {
-  if [ -x "$(command -v sts)" ]; then
+  if ! [ -x "$(command -v sts)" ]; then
     curl -o- https://dl.stackstate.com/stackstate-cli/install.sh | STS_CLI_LOCATION=/usr/local/bin bash
   else
     echo ">>> sts CLI already installed"
