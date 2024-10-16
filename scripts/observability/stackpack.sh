616d7d497876a42fb73cbfa30a347041c469d0c6
Bertrand THOMAS <bertrand@devpro.fr>
2024-10-16 13:46:05 +0200

    Feature/observability playground (#20)
    * Extra functions for automating playground installation
    
    Signed-off-by: Jeroen van Erp <jeroen.vanerp@suse.com>
    
    * Strip trailing slash if present
    
    Signed-off-by: Jeroen van Erp <jeroen.vanerp@suse.com>
    
    * Add extra cluster functions to ease multiple downstream cluster creation
    
    Signed-off-by: Jeroen van Erp <jeroen.vanerp@suse.com>
    
    * Fix code linting
    
    Signed-off-by: Jeroen van Erp <jeroen.vanerp@suse.com>
    
    ---------
    
    Signed-off-by: Jeroen van Erp <jeroen.vanerp@suse.com>
    Co-authored-by: Jeroen van Erp <jeroen.vanerp@suse.com>

--- scripts/observability/stackpack.sh
+++ scripts/observability/stackpack.sh
@@ -47,3 +47,48 @@ observability_check_stackpack() {
   [[ -n "$stackpack_id" ]]
   return
 }
+
+#######################################
+# Install a StackPack instance in SUSE Observability
+# Arguments:
+#   url (SUSE Observability)
+#   service_token (SUSE Observability)
+#   cluster_name
+# Examples:
+#   observability_install_stackpack https://obs.suse.com/ xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx demo
+#######################################
+observability_install_stackpack() {
+  local url=$1
+  local service_token=$2
+  local cluster_name=$3
+
+  local stackpacks
+  stackpacks=$(/usr/local/bin/sts stackpack list-instances --name kubernetes-v2 -o json --url $url --service-token $service_token)
+  if [[ $(echo $stackpacks | jq -r '.instances[] | select(.config.kubernetes_cluster_name == "'$cluster_name'") | .id') ]]; then
+    echo ">>> StackPack for cluster '${cluster_name}' already exists"
+  else
+    /usr/local/bin/sts stackpack install --name kubernetes-v2 --url $url --service-token $service_token -p "kubernetes_cluster_name=$cluster_name" --unlocked-strategy fail
+    echo ">>> StackPack for cluster '${cluster_name}' installed"
+  fi
+}
+
+#######################################
+# Get the status of a StackPack instance in SUSE Observability
+# Arguments:
+#   url (SUSE Observability)
+#   service_token (SUSE Observability)
+#   cluster_name
+# Output:
+#   The status of the StackPack instance
+# Examples:
+#   observability_stackpack_status https://obs.suse.com/ xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx demo
+#######################################
+observability_stackpack_status() {
+  local url=$1
+  local service_token=$2
+  local cluster_name=$3
+
+  local stackpacks
+  stackpacks=$(/usr/local/bin/sts stackpack list-instances --name kubernetes-v2 -o json --url $url --service-token $service_token)
+  echo $stackpacks | jq -r '.instances[] | select(.config.kubernetes_cluster_name == "'$cluster_name'") | .status'
+}
