216f77bf4426b567ab6ab301d636690d1ad66d3e
Bertrand Thomas <bertrand@devpro.fr>
2024-10-18 18:30:19 +0200

    Fix typo

--- scripts/linux/disk.sh
+++ scripts/linux/disk.sh
@@ -2,7 +2,7 @@
 # Collection of functions to work with disks on Linux
 
 #######################################
-# Create file and binds it to a loop device
+# Create a file and binds it to a loop device
 # Arguments:
 #   File name
 #   Size file (number of blocks)
@@ -15,9 +15,9 @@ linux_create_fileAndLoopDevice() {
   local sizeFile=$2
   local loopDevice=$3
 
-  echo "Creating local file ${fileName} and look device ${fileName}..."
+  echo "Creating local file ${fileName} and loop device ${fileName}..."
 
-  # prepare file for use with loopback device (creates a file filled with zero bytes)
+  # prepares the file for use with loopback device (creates a file filled with zero bytes)
   dd if=/dev/zero of=${fileName} bs=1M count=${sizeFile} status=progress
 
   # binds the file to the loop device (enabling to work with the file as if it were a block device, like a physical disk)
