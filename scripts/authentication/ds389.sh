7adf037d9ab2a1d8bc0ea2bbe1767f70e3a5e6f5
rmahique-github <>
2024-10-25 21:53:18 +0200

    turned into a library

--- scripts/authentication/ds389.sh
+++ scripts/authentication/ds389.sh
@@ -1,65 +1,80 @@
 #!/bin/bash
 
-
-# example:
-#_admin_user="cn=Directory Manager"
-#_admin_pwd="admin123"
-#_uri="ldap://node101.mydemo.lab:30389"
-#_connection_str="-D '${_admin_user}' -w '${_admin_pwd}' -x  -H '${_uri}'"
-#_basedn="dc=mydemo,dc=lab"
-#_ldap_user_dn="uid=ldap_user,ou=people,${_basedn}"
-#_ldap_user_pwd="supersecret123"
-
-
-
-
-## DS389: Restrict permissions
-## This will:
-## -  prevent normal users from reading the whole directory
+#######################################
+# DS389 - restrict permissions:
+#   - prevent normal users from reading the whole directory
+# Global vars:
+#   _ldap_uri
+#   _ldap_basedn
+#   _admin_user
+#   _admin_pwd
+# Examples:
+#   ds389_restrict_permissions
+#######################################
 function ds389_restrict_permissions() {
-  ldapmodify ${_connection_str} << EOL
-dn: ou=people,${_basedn}
+  ldapmodify -D "${_admin_user}" -w "${_admin_pwd}" -x  -H "${_ldap_uri}" << EOL
+dn: ou=people,${_ldap_basedn}
 changetype: modify
 delete: aci
 aci: (targetattr="objectClass || description || nsUniqueId || uid || displayName || loginShell || uidNumber || gidNumber || gecos || homeDirectory || cn || memberOf || mail || nsSshPublicKey || nsAccountLock || userCertificate")(targetfilter="(objectClass=posixaccount)")(version 3.0; acl "Enable anyone user read"; allow (read, search, compare)(userdn="ldap:///anyone");)
  
-dn: ou=people,${_basedn}
+dn: ou=people,${_ldap_basedn}
 changetype: modify
 add: aci
 aci: (targetattr="objectClass || description || nsUniqueId || uid || displayName || loginShell || uidNumber || gidNumber || gecos || homeDirectory || cn || memberOf || mail || nsSshPublicKey || nsAccountLock || userCertificate")(targetfilter="(objectClass=posixaccount)")(version 3.0; acl "Enable self user read"; allow (read, search, compare)(userdn="ldap:///self");)
 EOL
-
-
 }
 
-
-
-## DS389: Grant ldap_user privileges to read the whole directory 
+#######################################
+# DS389 - Grant user privileges to read the whole directory
+# Global vars:
+#   _ldap_uri
+#   _ldap_basedn
+#   _admin_user
+#   _admin_pwd
+# Arguments:
+#   1 - Username (Default: ldap_user)
+# Examples:
+#   ds389_user_private_read [<a_user>]
+#######################################
 function ds389_ldap_user-user_private_read() {
-  ldapmodify  ${_connection_str} << EOL
-dn: cn=user_private_read,ou=permissions,${_basedn}
+  ldapmodify -D "${_admin_user}" -w "${_admin_pwd}" -x  -H "${_ldap_uri}" << EOL
+dn: cn=user_private_read,ou=permissions,${_ldap_basedn}
 changetype: modify
 add: member
-member: uid=ldap_user,ou=people,${_basedn}
+member: uid=${1:-ldap_user},ou=people,${_ldap_basedn}
 EOL
-
 }
 
-
-## DS389: Verify ldap_user has access
-function ds389_ldap_user-access() {
-  ldapsearch -x  -D "${_ldap_user_dn}" -w "${_ldap_user_pwd}" -H "${_uri}"  -b "${_basedn}"
+#######################################
+# DS389 - Verify user has access
+# Global vars:
+#   _ldap_uri
+#   _ldap_basedn
+# Arguments:
+#   1 - ldap user DN
+#   2 - ldap user pwd
+# Examples:
+#   ds389_ldap_user_access_check "uid=ldap_user,ou=people,dc=mydemo,dc=lab" "mypassword"
+#######################################
+function ds389_ldap_user_access_check() {
+  ldapsearch -x  -D "${1}" -w "${2}" -H "${_ldap_uri}"  -b "${_ldap_basedn}"
 }
 
-
-
-## DS389: Install 389 Directory server
+#######################################
+# DS389 - Install 389 Directory server
+# Global vars:
+#   _ldap_uri
+#   _ldap_basedn
+#   _admin_user
+#   _admin_pwd
+# Examples:
+#   ds389_install
+#######################################
 function ds389_install() {
-
   # add the repo
   helm repo add suse-lab-setup https://opensource.suse.com/lab-setup
   helm repo update
-
   # installs the chart with default parameters
   if [[ -f values.yaml ]]
   then
@@ -67,87 +82,20 @@ function ds389_install() {
   else
     helm upgrade --install ds389 suse-lab-setup/ds389
   fi
-
   sleep 60
   ds389_restrict_permissions
   ds389_ldap_user-user_private_read
 }
 
-
-## DS389: uninstall 389 Directory server
+#######################################
+# DS389 - Uninstall 389 Directory server
+# Arguments:
+#   1 - Namespace (Default: ds389)
+#   2 - App_name (Default: ds389)
+# Examples:
+#   ds389_uninstall [<ds389> <ds389>]
+#######################################
 function ds389_uninstall() {
-  kubectl -n ds389  delete ServiceAccount/ds389-sa Secret/dirsrv-tls-secret Secret/dirsrv-dm-password StatefulSet/ds389 Ingress/ds389 Service/ds389 Namespace/ds389 service/ds389-internal-svc  service/ds389-external-svc; sleep 15 
+  kubectl -n ${1:-ds389} delete ServiceAccount/${2:-ds389}-sa Secret/dirsrv-tls-secret Secret/dirsrv-dm-password StatefulSet/${2:-ds389} Ingress/${2:-ds389} Service/${2:-ds389} service/${2:-ds389}-internal-svc  service/${2:-ds389}-external-svc Namespace/${1:-ds389}
+  sleep 15 
 }
-
-
-function usage() {
-        echo "Usage:
-$0 [install|uninstall|create-user|delete-user|create-group|delete-group|check-user|check-group|change-passwd] <[user|group]> <password>"
-
-}
-
-
-_arg="$1"
-
-
-if [[ ! ${_arg} ]]
-then
-        echo "ERROR: Missing argument"
-        usage
-        exit 1
-fi
-
-case ${_arg} in
-  install)
-    ds389_install
-    ;;
-  uninstall)
-    ds389_uninstall
-    ;;
-  create-user)
-    [[ "$2" == "" ]] && ( echo "ERROR: Missing user"; usage ;  exit 1)
-    # these are only for rancher, therefore doesn't matter if they have the same UID
-    dsidm localhost --basedn "${_basedn}" user create --uid $2 --cn $2 --displayName $2 --uidNumber 1001 --gidNumber 1001 --homeDirectory /home/${2}
-    ;;
-  delete-user)
-    [[ "$2" == "" ]] && ( echo "ERROR: Missing user"; usage ;  exit 1)
-    ldapmodify ${_connection_str} << EOL
-dn: ou=people,${_basedn}
-changetype: modify
-delete: uid
-uid: $2
-EOL
-    ;;
-  create-group)
-    [[ "$2" == "" ]] && ( echo "ERROR: Missing group"; usage ;  exit 1)
-    dsidm localhost --basedn "${_basedn}" group create --cn $2 ; 
-    ;;
-  delete-group)
-    [[ "$2" == "" ]] && ( echo "ERROR: Missing group"; usage ;  exit 1)
-    ldapmodify ${_connection_str} << EOL
-dn: ou=group,${_basedn}
-changetype: modify
-delete: cn
-cn: $2
-EOL
-    ;;
-  check-user)
-    [[ "$2" == "" ]] && ( echo "ERROR: Missing user"; usage ;  exit 1)
-    dsidm localhost --basedn "${_basedn}" account get-by-dn uid=${2},ou=people,${_basedn}
-    ;;
-  check-group)
-    [[ "$2" == "" ]] && ( echo "ERROR: Missing group"; usage ;  exit 1)
-    dsidm localhost --basedn "${_basedn}" account get-by-dn uid=${2},ou=group,${_basedn}
-    ;;
-  change-passwd)
-    [[ "$2" == "" ]] && ( echo "ERROR: Missing user"; usage ;  exit 1)
-    [[ "$3" == "" ]] && ( echo "ERROR: Missing password"; usage ;  exit 1)
-    dsidm localhost -b "${_basedn}"  account change_password uid=${2},ou=people,dc=mydemo,dc=lab $3 
-    ;;
-  *)
-    usage
-    exit 1
-    ;;
-esac
-
-
