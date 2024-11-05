82fea318ba7fe9f1664ad53b6e60d669eac8a320
Jeroen van Erp <jeroen.vanerp@suse.com>
2024-11-05 11:07:15 +0100

    Fixup helm charts and add cli install
    Signed-off-by: Jeroen van Erp <jeroen.vanerp@suse.com>

--- scripts/observability/cli.sh
+++ scripts/observability/cli.sh
@@ -5,7 +5,7 @@
 #######################################
 observability_install_cli() {
   if ! [ -x "$(command -v sts)" ]; then
-    curl -o- https://dl.stackstate.com/stackstate-cli/install.sh | STS_CLI_LOCATION=/usr/local/bin bash
+    curl -s -o- https://dl.stackstate.com/stackstate-cli/install.sh | STS_CLI_LOCATION=/usr/local/bin bash
   else
     echo ">>> sts CLI already installed"
   fi
