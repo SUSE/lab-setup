1affbcbaf1d4243c39e8179b236e12c042e45c1a
Jeroen van Erp <jeroen.vanerp@suse.com>
2024-11-07 12:31:22 +0100

    Switched parameters
    Signed-off-by: Jeroen van Erp <jeroen.vanerp@suse.com>

--- scripts/observability/monitors.sh
+++ scripts/observability/monitors.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 observability_disable_monitor() {
-    local url=$1
-    local service_token=$2
-    local monitor_identifier=$3
+    local monitor_identifier=$1
+    local url=$2
+    local service_token=$3
     /usr/local/bin/sts monitor disable --identifier $monitor_identifier --service-token $service_token --url $url
 }
 
 observability_deploy_monitor() {
-    local url=$1
-    local service_token=$2
-    local file $3
+    local file $1
+    local url=$2
+    local service_token=$3
     /usr/local/bin/sts monitor apply -f $file --service-token $service_token --url $url
 }
 
 observability_enable_monitor() {
-    local url=$1
-    local service_token=$2
-    local monitor_identifier=$3
+    local monitor_identifier=$1
+    local url=$2
+    local service_token=$3
     /usr/local/bin/sts monitor enable --identifier $monitor_identifier --service-token $service_token --url $url
 }
 
