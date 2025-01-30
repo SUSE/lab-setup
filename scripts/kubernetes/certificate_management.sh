58346bbe0cb3a290137211e884be398c2f532575
Jeroen van Erp <jeroen.vanerp@suse.com>
2025-01-30 16:09:52 +0100

    Add checks for setup
    Signed-off-by: Jeroen van Erp <jeroen.vanerp@suse.com>

--- scripts/kubernetes/certificate_management.sh
+++ scripts/kubernetes/certificate_management.sh
@@ -15,9 +15,17 @@ k8s_install_certmanager() {
   helm repo add jetstack https://charts.jetstack.io
   helm repo update
   kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/${version}/cert-manager.crds.yaml
+  if $? -ne 0; then
+    echo "Failed to install cert-manager CRDs"
+    exit 1
+  fi
   helm upgrade --install cert-manager jetstack/cert-manager \
     --namespace cert-manager --create-namespace \
     --version ${version}
+  if $? -ne 0; then
+    echo "Failed to install cert-manager"
+    exit 1
+  fi
   kubectl wait pods -n cert-manager -l app.kubernetes.io/instance=cert-manager --for condition=Ready 2>/dev/null
 }
 
@@ -40,6 +48,10 @@ k8s_create_letsencryptclusterissuer() {
     --namespace cert-manager \
     --set ingress.className=${ingressClassname} \
     --set registration.emailAddress=${emailAddress}
+  if $? -ne 0; then
+    echo "Failed to create Let's Encrypt cluster issuer"
+    exit 1
+  fi
   sleep 5
   while kubectl get clusterissuers -o json | jq -e '.items[] | select(.status.conditions[] | select(.type == "Ready" and .status != "True"))' > /dev/null; do
     sleep 1
