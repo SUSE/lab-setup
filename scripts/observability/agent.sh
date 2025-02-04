17fada2dda6298c35c00e36f4081426615d2748f
Jeroen van Erp <jeroen.vanerp@suse.com>
2025-02-04 16:26:30 +0100

    added nowait
    Signed-off-by: Jeroen van Erp <jeroen.vanerp@suse.com>

--- scripts/observability/agent.sh
+++ scripts/observability/agent.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 #######################################
-# Install the Observability agent in the cluster
+# Install the Observability agent in the cluster and not wait for the pods to be ready
 # Arguments:
 #   url (SUSE Observability)
 #   cluster_name
 #   ingestion_api_key
 # Examples:
-#   observability_agent_install https://obs.suse.com demo xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
+#   observability_agent_install_nowait https://obs.suse.com demo xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
 #######################################
-observability_agent_install() {
+observability_agent_install_nowait() {
     local url=$1
     local cluster_name=$2
     local ingestion_api_key=$3
@@ -22,6 +22,23 @@ observability_agent_install() {
         --set stackstate.apiKey=${ingestion_api_key} \
         --set stackstate.url="${url%/}/receiver/stsAgent" \
         --set stackstate.cluster.name=${cluster_name}
+}
 
-    kubectl wait pods -n suse-observability -l app.kubernetes.io/instance=suse-observability-agent --for condition=Ready 2>/dev/null
+#######################################
+# Install the Observability agent in the cluster
+# Arguments:
+#   url (SUSE Observability)
+#   cluster_name
+#   ingestion_api_key
+# Examples:
+#   observability_agent_install https://obs.suse.com demo xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
+#######################################
+observability_agent_install() {
+    local url=$1
+    local cluster_name=$2
+    local ingestion_api_key=$3
+
+    observability_agent_install_nowait $url $cluster_name $ingestion_api_key
+
+    kubectl wait pods -n suse-observability -l app.kubernetes.io/instance=suse-observability-agent --for condition=Ready
 }
