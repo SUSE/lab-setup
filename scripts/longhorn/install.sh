6307586f0f22be5e6fb6f4a34bd78668e95c5733
Jeroen van Erp <jeroen.vanerp@suse.com>
2025-03-20 11:36:50 +0100

    Fix longhorn install
    Signed-off-by: Jeroen van Erp <jeroen.vanerp@suse.com>

--- scripts/longhorn/install.sh
+++ scripts/longhorn/install.sh
@@ -9,7 +9,9 @@ longhorn_install() {
     systemctl enable --now iscsid.service
     modprobe iscsi_tcp
     echo '=== Check prerequisites'
-    curl -k https://raw.githubusercontent.com/longhorn/longhorn/refs/heads/master/scripts/environment_check.sh 2>/dev/null | bash || exit 1
+    curl -k -sSfL -o longhornctl https://github.com/longhorn/cli/releases/download/v1.8.1/longhornctl-linux-amd64
+    chmod +x longhornctl
+    ./longhornctl check preflight
     echo '=== Install LongHorn'
     helm upgrade -i longhorn longhorn/longhorn --namespace longhorn-system --create-namespace --set ingress.enabled=true --set ingress.host=$hostname --set persistence.migratable=true --set longhornUI.replicas=1
     echo "<<< Longhorn should be available in a few minutes in: $hostname"
