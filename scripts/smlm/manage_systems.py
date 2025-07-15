5719bc2608b9d044e90b6153a13197a63ffb4787
rmahique-github <>
2025-07-15 23:43:27 +0200

    extended manage_systems with new options

--- scripts/smlm/manage_systems.py
+++ scripts/smlm/manage_systems.py
@@ -7,8 +7,7 @@
 #
 
 from xmlrpc.client import ServerProxy
-import ssl, argparse, yaml, sys
-
+import ssl, argparse, yaml, sys, datetime
 
 
 def smlm_login(server, user, pwd):
@@ -59,6 +58,35 @@ def smlm_delete_system(key, client, sid):
   return systems
 
 
+def smlm_schedule_applyHighstate(key, client, sid, when, test=False):
+  """
+  Schedule highstate application for a given system.
+  """
+  if not when:
+    earliest_occurrence = datetime.datetime.now(datetime.timezone.utc)
+  else:
+    earliest_occurrence = when
+  results = client.system.scheduleApplyHighstate(key, sid, earliest_occurrence, test )
+  return results
+
+def smlm_schedule_packageRefresh(key, client, sid, when ):
+  """
+  Schedule a package list refresh for a system.
+  """
+  if not when:
+    earliest_occurrence = datetime.datetime.now(datetime.timezone.utc)
+  else:
+    earliest_occurrence = when
+  results = client.system.schedulePackageRefresh(key, sid, earliest_occurrence )
+  return results
+
+def smam_system_getDetails(key, client, sid):
+  """
+  Get system details.
+  """
+  results = client.system.getDetails(key, sid)
+  return results
+
 
 def main():
   """
@@ -70,12 +98,16 @@ def main():
   group.add_argument('--addcustomvalue', action='store_true', help='Add custom value to a system')
   group.add_argument('-d','--delete', action='store_true', help='Delete system')
   group.add_argument('-l','--list', action='store_true', help='List all system groups')
+  group.add_argument('--scheduleapplyhighstate', action='store_true', help='Schedule highstate application for a given system.')
+  group.add_argument('--schedulepackagerefresh', action='store_true', help='Schedule a package list refresh for a system.')
+  group.add_argument('-g','--getinfo', action='store_true', help='Get system info')
   parser.add_argument('-u', '--user', type=str, required=True, help='User name with privileges to create other users')
   parser.add_argument('-p', '--pwd', type=str, required=True, help='Password for user')
   parser.add_argument('-s', '--server', type=str, required=True, help='Server FQDN')
   parser.add_argument('--customkey', type=str, help='Custom key name.')
   parser.add_argument('--system', type=str, help='System name.')
   parser.add_argument('--keyvalue', type=str, help="Custom key value.")
+  parser.add_argument('--executiondate', type=str, help="Date for the action to be executed, default is now, format: " + str(datetime.datetime.now(datetime.timezone.utc)) +".")
   parser.add_argument('--debug', action='store_true', help="Use it to enable debug messages.")
   inputparam=parser.parse_args()
 
@@ -84,6 +116,7 @@ def main():
 
   key, client = smlm_login(inputparam.server, inputparam.user, inputparam.pwd)
 
+
   if(inputparam.addcustomvalue):
     if( not inputparam.customkey or not inputparam.keyvalue or not inputparam.system):
       sys.exit("Missing parameters, --customkey, --system, --keyvalue are required")
@@ -91,6 +124,8 @@ def main():
     results=smlm_get_systemid(key, client, inputparam.system)
     smlm_add_customvalues(key, client, results[0]['id'], inputparam.customkey, inputparam.keyvalue)
   elif(inputparam.delete):
+    if( not inputparam.system):
+      sys.exit("Missing parameters, --system is required")
     print("Deleting system " + str(inputparam.system))
     results=smlm_get_systemid(key, client, inputparam.system)
     sid=results[0]['id']
@@ -98,6 +133,24 @@ def main():
   elif(inputparam.list):
     print("Retrieving the list of systems")
     print(yaml.dump(smlm_list_systems(key, client)))
+  elif(inputparam.scheduleapplyhighstate):
+    if( not inputparam.system):
+      sys.exit("Missing parameters, --system is required")
+    print("Scheduling apply highstate for system " + str(inputparam.system))
+    systemid=smlm_get_systemid(key, client, inputparam.system)
+    results=smlm_schedule_applyHighstate(key, client, systemid[0]['id'], inputparam.executiondate, test=False)
+  elif(inputparam.schedulepackagerefresh):
+    if( not inputparam.system):
+      sys.exit("Missing parameters, --system is required")
+    print("Scheduleing a package refresh for system " + str(inputparam.system))
+    systemid=smlm_get_systemid(key, client, inputparam.system)
+    results=smlm_schedule_packageRefresh(key, client, systemid[0]['id'], inputparam.executiondate)
+  elif(inputparam.getinfo):
+    if( not inputparam.system):
+      sys.exit("Missing parameters, --system is required")
+    print("Get information about system " + str(inputparam.system))
+    systemid=smlm_get_systemid(key, client, inputparam.system)
+    print(yaml.dump(smam_system_getDetails(key, client, systemid[0]['id'])))
   else:
     print("Invalid or missing parameters")
     if(inputparam.debug):
