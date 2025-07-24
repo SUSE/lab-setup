23475fe6aa6d4f3544254b196acc526b6b997346
Jeroen van Erp <jeroen.vanerp@suse.com>
2025-07-24 23:45:56 +0200

    Fix ingestion-api-key -> service-token

--- scripts/observability/api_key.sh
+++ scripts/observability/api_key.sh
@@ -17,33 +17,9 @@ observability_create_ingestion_api_key() {
   local cluster_name=$3
 
   local resp
-  resp=$(/usr/local/bin/sts ingestion-api-key create --name $cluster_name -o json --url $url --service-token $service_token)
+  /usr/local/bin/sts rbac create-subject --subject $cluster_name-agent
+  /usr/local/bin/sts rbac grant --subject $cluster_name-agent --permission update-metrics
+  resp=$(/usr/local/bin/sts service-token create --name $PROD_CLUSTER_NAME --roles $PROD_CLUSTER_NAME-agent --service-token $service_token --url $url -o json)
 
-  echo $resp | jq -r '."ingestion-api-key".apiKey'
-}
-
-#######################################
-# Delete an Ingestion API key for SUSE Observability
-# Arguments:
-#   url (SUSE Observability)
-#   service_token (SUSE Observability)
-#   cluster_name
-# Examples:
-#   observability_delete_ingestion_api_key https://obs.suse.com/ xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx demo
-#######################################
-observability_delete_ingestion_api_key() {
-  local url=$1
-  local service_token=$2
-  local cluster_name=$3
-
-  local keys key_id
-
-  keys=$(/usr/local/bin/sts ingestion-api-key list -o json --url $url --service-token $service_token)
-  key_id=$(echo $keys | jq -r '."ingestion-api-keys"[] | select(.name == "'$cluster_name'") | .id')
-  if [ -n "$key_id" ]; then
-    /usr/local/bin/sts ingestion-api-key delete --id $key_id --url $url --service-token $service_token
-    echo ">>> Ingestion API key for cluster '${cluster_name}' deleted"
-  else
-    echo ">>> Ingestion API key for cluster '${cluster_name}' not found"
-  fi
+  echo $resp | jq -r '."service-token".token'
 }
